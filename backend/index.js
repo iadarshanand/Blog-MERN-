ee786576476f2ae7327407dd5c6ff4b2ab0aa271
Adarsh Anand <anandadarsh2511@gmail.com>
2023-12-21 17:36:52 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,16 @@ app.use(
     optionsSuccessStatus: 204, // Important for preflight requests
   })
 );
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
